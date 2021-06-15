7f00bfabd1b3d0243f0e901fccfebe5f1791349f
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-06-15 21:16:49 +0900

    Reactoring core feature #17

---
+++ src/kr.bbon.AspNetCore/HealthCheckBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace kr.bbon.AspNetCore
+{
+    public abstract class HealthCheckBase : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return await CheckAsync(cancellationToken);
+        }
+
+        public abstract Task<HealthCheckResult> CheckAsync(CancellationToken cancellationToken = default);
+    }
+}
